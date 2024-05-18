3b6177e81dc1d50d46214fe67348ee362b7d4b40
pavlo-boichuk <betonsuccess@gmail.com>
2024-05-19 00:56:46 +0300

    upd 2

--- task_2_process/with_process.py
+++ task_2_process/with_process.py
@@ -1,47 +1,23 @@
 import datetime
-from random import randint
-from multiprocessing import Process, cpu_count
-import sys
+from multiprocessing import cpu_count, Pool
 
 
 def factorize(*number):
-    result = []
     for num in number:
-        result.append([n for n in range(1, num+1) if num%n==0])
+        return [n for n in range(1, num+1) if num%n==0]
 
+
+def callback(result):
     print(result)
-    sys.exit(0)
 
 
 if __name__ == '__main__':
+    start_time = datetime.datetime.now()
 
-    if cpu_count() > 1:
-        start_time = datetime.datetime.now()
+    with Pool(processes=cpu_count()) as pool:
+        pool.map_async(factorize, [128, 255, 99999, 10651060], callback=callback)
+        pool.close()
+        pool.join()
 
-        process = []
-    
-        pr1 = Process(target=factorize, args=(128, 255))
-        pr1.start()
-        process.append(pr1)
-
-        pr2 = Process(target=factorize, args=(99999, 10651060))
-        pr2.start()
-        process.append(pr2)
-
-        [pr.join() for pr in process]
-        [print(pr.exitcode, end=' ') for pr in process]
-        print('')
-
-        end_time = datetime.datetime.now()
-        print(f'result time for 2 process: {end_time - start_time}')
-
-    # process_count = randint(1, cpu_count())
-    # process = []
-    # for i in range(process_count):
-    #     pr = Process(target=factorize, args=(128, 255, 99999, 10651060))
-    #     pr.start()
-    #     process.append(pr)
-
-    # [pr.join() for pr in process]
-    # [print(pr.exitcode, end=' ')]
-    # print('')
+    end_time = datetime.datetime.now()
+    print(f'result time for processes: {end_time - start_time}')
